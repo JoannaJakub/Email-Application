4142f9b8873ea7f2bcf4d5575ffe40465c43fa3d
JoannaJakub <joannajakubczyk@interia.pl>
2021-10-02 21:21:12 +0200

    starter

--- src/main/java/emailapp/Email.java
+++ src/main/java/emailapp/Email.java
@@ -1,5 +1,13 @@
 package emailapp;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
 @Entity
 @Data
 @Component
@@ -7,9 +15,14 @@ package emailapp;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Email {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String firstName;
     private String lastName;
     private String departament;
     private int mailboxCapacity;
     private String alternateEmail;
+
 }
--- src/main/java/emailapp/EmailApp.java
+++ src/main/java/emailapp/EmailApp.java
@@ -1,4 +1,14 @@
 package emailapp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class EmailApp {
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "hello world !!!";
+    }
 }
