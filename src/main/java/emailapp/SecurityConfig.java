01ccfacdaaf976211b82986c580c60c537ca291d
JoannaJakub <joannajakubczyk@interia.pl>
2021-10-08 16:23:19 +0200

    login

--- src/main/java/emailapp/SecurityConfig.java
+++ src/main/java/emailapp/SecurityConfig.java
@@ -16,5 +16,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .antMatchers("/landingPage").authenticated()
                 .and().formLogin();
+        //
     }
 }
