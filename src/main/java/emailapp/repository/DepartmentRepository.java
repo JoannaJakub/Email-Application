f09a7c7941143082dcb78c845c8331a86139a0ab
JoannaJakub <joannajakubczyk@interia.pl>
2021-10-08 17:38:48 +0200

    pl package

--- src/main/java/emailapp/repository/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package emailapp.repository;
-
-import emailapp.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
--- src/main/java/emailapp/repository/EmailRepository.java
+++
@@ -1,9 +0,0 @@
-package emailapp.repository;
-
-import emailapp.model.Email;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EmailRepository extends JpaRepository<Email, Long> {
-    Email findTopByOrderByIdDesc();
-}
