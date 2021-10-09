c9f7ee7bc9f4bbd63868dbfad4aefe1702954e70
JoannaJakub <joannajakubczyk@interia.pl>
2021-10-09 14:50:41 +0200

    user service

---
+++ src/main/java/emailapp/service/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package emailapp.service;
+
+import emailapp.model.Role;
+import emailapp.model.User;
+import emailapp.repository.RoleRepository;
+import emailapp.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository,
+                           BCryptPasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+    @Override
+    public User findByUserName(String username) {
+        return userRepository.findByUsername(username);
+    }
+    @Override
+    public void saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setEnabled(false);
+        Role userRole = roleRepository.findByName("ROLE_USER");
+        user.setRole(new HashSet<Role>(Arrays.asList(userRole)));
+        userRepository.save(user);
+    }
+}
