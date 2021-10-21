dc330bf2c778435168044da68b4f053a4b456e6f
JoannaJakub <joannajakubczyk@interia.pl>
2021-10-21 20:51:17 +0200

    add user error

--- src/main/java/emailapp/controller/UsersController.java
+++ src/main/java/emailapp/controller/UsersController.java
@@ -1,5 +1,6 @@
 package emailapp.controller;
 
+import emailapp.RandomPasswordGenerator;
 import emailapp.model.User;
 import emailapp.repository.EmailRepository;
 import emailapp.repository.RoleRepository;
@@ -21,12 +22,15 @@ public class UsersController {
     public final EmailRepository emailRepository;
     public final RoleRepository roleRepository;
     public final UserService userService;
+    public final RandomPasswordGenerator randomPasswordGenerator;
 
-    public UsersController(UserRepository userRepository, EmailRepository emailRepository, RoleRepository roleRepository, UserService userService) {
+
+    public UsersController(UserRepository userRepository, EmailRepository emailRepository, RoleRepository roleRepository, UserService userService, RandomPasswordGenerator randomPasswordGenerator) {
         this.userRepository = userRepository;
         this.emailRepository = emailRepository;
         this.roleRepository = roleRepository;
         this.userService = userService;
+        this.randomPasswordGenerator = randomPasswordGenerator;
     }
 
     @RequestMapping("/allUsers")
@@ -44,17 +48,19 @@ public class UsersController {
 
 
     @PostMapping(value = "/addUserSuccess")
-    public String addUserSuccess(@Valid User user, BindingResult result, Model model) {
+    public String addUserSuccess(@Valid User user, BindingResult result) {
+        user.setPassword(randomPasswordGenerator.generatePassword());
         if (result.hasErrors()) {
-            model.addAttribute("role", roleRepository.findAll());
+            System.out.println("this is userErrro......." + user);
             return "admin/user/addUser";
         } else if (userService.findByUserName(user.getUsername().toLowerCase()) != null) {
             result.addError(new FieldError(user.toString(), "username", "Email is already taken"));
         } else {
+            System.out.println("this is user......." + user);
             userService.saveUser(user);
-            return "admin/user/userSuccess";
+
         }
-        return "admin/user/addUser";
+        return "admin/user/userSuccess";
     }
 
     @RequestMapping("/usersConfirmDelete")
--- src/main/java/emailapp/repository/UserRepository.java
+++ src/main/java/emailapp/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package emailapp.repository;
 
-import emailapp.model.Department;
 import emailapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
