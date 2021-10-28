7bac1d82f6c8c6b602b5735500458bde0a73d6e9
JoannaJakub <joannajakubczyk@interia.pl>
2021-10-28 19:04:30 +0200

    password

--- src/main/java/emailapp/model/User.java
+++ src/main/java/emailapp/model/User.java
@@ -43,4 +43,5 @@ public class User implements Serializable {
 
     @Column(name = "reset_password_token")
     private String resetPasswordToken;
+    private String email;
 }
--- src/main/java/emailapp/service/UserNotFoundException.java
+++ src/main/java/emailapp/service/UserNotFoundException.java
@@ -1,6 +1,7 @@
 package emailapp.service;
 
 public class UserNotFoundException extends Exception {
-    public UserNotFoundException(String s) {
+    public UserNotFoundException(String message) {
+        super(message);
     }
 }
