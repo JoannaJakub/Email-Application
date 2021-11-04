eb5c1997fd4505f5bedd4a3de255a39c54c0d53e
JoannaJakub <joannajakubczyk@interia.pl>
2021-11-04 17:52:04 +0100

    create event

--- src/main/java/emailapp/controller/EventController.java
+++ src/main/java/emailapp/controller/EventController.java
@@ -7,8 +7,11 @@ import emailapp.repository.EventRepository;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 
 
@@ -19,15 +22,37 @@ public class EventController {
     public EventController(EventRepository eventRepository) {
         this.eventRepository = eventRepository;
     }
+
+    @GetMapping("/event")
+    public String event() {
+        return "admin/event/calendar";
+    }
+
     @GetMapping("/api/events")
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     Iterable<Event> events(@RequestParam("start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start, @RequestParam("end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end) {
         return eventRepository.findBetween(start, end);
     }
 
-    @GetMapping("/event")
-    public String event() {
-        return "admin/event/calendar";
+    @PostMapping("/api/events/create")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @Transactional
+    Event createEvent(@RequestBody EventCreateParams params) {
+
+        Event e = new Event();
+        e.setStart(params.start);
+        e.setEnd(params.end);
+        e.setText(params.text);
+
+        eventRepository.save(e);
+        return e;
+    }
+
+    public static class EventCreateParams {
+        public LocalDateTime start;
+        public LocalDateTime end;
+        public String text;
+        public Long resource;
     }
 
 }
