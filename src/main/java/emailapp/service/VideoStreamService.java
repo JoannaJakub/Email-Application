690109b4569d564363e47f99b04e1a1aca3b2782
JoannaJakub <joannajakubczyk@interia.pl>
2021-11-28 10:11:18 +0100

    video stream - const, get, read file

---
+++ src/main/java/emailapp/service/VideoStreamService.java
@@ -0,0 +1,46 @@
+package emailapp.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import static emailapp.VideoConst.*;
+
+
+@Service
+public class VideoStreamService {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private String getFilePath() {
+        URL url = this.getClass().getResource(VIDEO);
+        return new File(url.getFile()).getAbsolutePath();
+    }
+
+    public Long getFileSize(String fileName) {
+        return Optional.ofNullable(fileName)
+                .map(file -> Paths.get(getFilePath(), file))
+                .map(this::sizeFromFile)
+                .orElse(0L);
+    }
+
+    private Long sizeFromFile(Path path) {
+        try {
+            return Files.size(path);
+        } catch (IOException ioException) {
+            logger.error("Error while getting the file size", ioException);
+        }
+        return 0L;
+    }
+}
